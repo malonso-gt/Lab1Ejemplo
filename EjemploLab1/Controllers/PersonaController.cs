84084698dbb44b3d70dfa8b7d238b291fb392229
maalonsg@correo.url.edu.gt <maag2301>
2018-02-01 08:36:37 -0600

    Ejemplo 1

---
+++ EjemploLab1/Controllers/PersonaController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EjemploLab1.DBContext;
+using EjemploLab1.Models;
+
+namespace EjemploLab1.Controllers
+{
+
+    public class PersonaController : Controller
+    {
+
+        DefaultConnection db = DefaultConnection.getInstance;
+       
+        //
+        // GET: /Persona/
+        public ActionResult Index()
+        {
+            return View(db.Personas.ObtenerListado());
+        }
+
+        //
+        // GET: /Persona/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /Persona/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Persona/Create
+        [HttpPost]
+        public ActionResult Create([Bind(Include = "PersonaID,Nombre,Apellido,Edad")] Persona persona)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                db.Personas.Insertar(persona);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Persona/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Persona/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Persona/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Persona/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ EjemploLab1/Models/Persona.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EjemploLab1.Models
+{
+    public class Persona
+    {
+        public int PersonaID { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; }
+
+        public int Edad { get; set; }
+    }
+}
